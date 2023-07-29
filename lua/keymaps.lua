9eef16e336bc9f0c19c9b52e3d54d73a9dd41110
ButterBlock233 <Voltage15312@outlook.com>
2023-07-29 20:56:02 +0800

    init

---
+++ lua/keymaps.lua
@@ -0,0 +1,43 @@
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set('n', '<M-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<M-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<M-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<M-l>', '<C-w>l', opts)
+
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+
+
+
+vim.keymap.set('i','<C-h>','<Left>',opts)
+vim.keymap.set('i','<C-j>','<Down>',opts)
+vim.keymap.set('i','<C-k>','<Up>',opts)
+vim.keymap.set('i','<C-l>','<Right>',opts)
+
+--vim.keymap.set('i','<>','<>',opts)
+
+
+
+-----------------
+-- Visual mode --
+-----------------
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
