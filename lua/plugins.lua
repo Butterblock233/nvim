8fec6bb9e2a42006d7989eac4e0426d59ab6bfa2
ButterBlock233 <Voltage15312@outlook.com>
2023-07-30 12:45:47 +0800

    update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,13 +3,13 @@ require('lazy').setup({
    "hrsh7th/nvim-cmp";
    "williamboman/mason.nvim";
    "neovim/nvim-lspconfig";
+   "folke/noice.nvim";
    {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",}
+      "MunifTanjim/nui.nvim";}
     }
-    "folke/noice.nvim"
 })
