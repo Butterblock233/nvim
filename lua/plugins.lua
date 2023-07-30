54801ebb4997029daa51cfd8e89cc5077d7df413
Butter Block <Voltage15312@outlook.com>
2023-07-30 18:32:43 +0800

    新UI

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,5 +11,15 @@ require('lazy').setup({
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim";}
+    };
+    {
+        "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",}
     }
 })
