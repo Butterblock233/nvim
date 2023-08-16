f2fb2f20800116aa46b161909c8afabd11d28fde
ButterBlock233 <Voltage15312@outlook.com>
2023-08-16 22:22:30 +0800

    整理快捷键

---
+++ lua/keymaps/editor/keymaps.lua
@@ -0,0 +1,32 @@
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+map('n', '<M-h>', '<C-w>h', opts)
+map('n', '<M-j>', '<C-w>j', opts)
+map('n', '<M-k>', '<C-w>k', opts)
+map('n', '<M-l>', '<C-w>l', opts)
+
+
+
+--  快速保存 --
+map('i','<C-s>','<cmd>w<CR>',opts)
+map('n','<C-s>',':w<CR>')
+
+
+--窗口移动--
+map('i','<C-h>','<Left>',opts)
+map('i','<C-j>','<Down>',opts)
+map('i','<C-k>','<Up>',opts)
+map('i','<C-l>','<Right>',opts)
+
+--<C-w>关闭窗口--
+map('i','<C-w>','<cmd>q')
+map('n','<C-w>','<cmd>q')
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
---
+++ lua/keymaps/plugin/keymaps.lua
@@ -0,0 +1,3 @@
+-- 插件设置 -- 
+map('n','<C-b>',':Neotree<CR>',opts)
+map('i','<C-b>','<cmd>Neotree<CR>',opts)
