75017cc94768922d1eaa5c6994c600138f7c1cdb
ButterBlock233 <Voltage15312@outlook.com>
2023-08-16 22:23:15 +0800

    更新插件

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -3,7 +3,9 @@ require('lazy').setup({
    "hrsh7th/nvim-cmp";
    "williamboman/mason.nvim";
    "neovim/nvim-lspconfig";
-   "folke/noice.nvim";
+    "nvim-telescope/telescope.nvim",
+    "yamatsum/nvim-nonicons",
+    "williamboman/mason-lspconfig.nvim",
    {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -12,7 +14,9 @@ require('lazy').setup({
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim";}
     };
-    {
+  --"neoclide/coc.nvim",
+	--"clangd/coc-clangd",
+  --[[{
         "folke/noice.nvim",
       event = "VeryLazy",
       opts = {
@@ -21,5 +25,5 @@ require('lazy').setup({
       dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",}
-    }
+    }] --]]
 })
