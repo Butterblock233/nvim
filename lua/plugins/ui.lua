eebfb93982a597f79db6869624eeb9beb81f0be8
ButterBlock233 <Voltage15312@outlook.com>
2024-07-04 14:18:43 +0800

    优化bufferline启动行为

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,7 +15,7 @@ return {
       { "[b",         "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev buffer" },
       { "]b",         "<cmd>BufferLineCycleNext<cr>",            desc = "Next buffer" },
     },
-    config = function()
+    init = function()
       require('bufferline').setup {}
     end
   },
