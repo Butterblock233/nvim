2565342360e64d2fe381a394dd40b02e33f9a6c4
Butterblock233 <Voltage15312@outlook.com>
2024-10-16 00:20:35 +0800

    Update many plugins(

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,11 +1,121 @@
 return {
-  {
-    "dstein64/vim-startuptime",
-    -- lazy-load on a command
-    cmd = "StartupTime",
-    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
-    init = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
+	{
+		-- vim-startuptime
+		-- Desc:eveluate nvim's startup time
+		"dstein64/vim-startuptime",
+		-- lazy-load on a command
+		cmd = "StartupTime",
+		-- init is called during startup. Configuration for vim plugins typically should be set in an init function
+		init = function()
+			vim.g.startuptime_tries = 10
+		end,
+	},
+	{
+		-- which-key.nvim
+		-- Desc: hint Keymaps
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		-- nvim-autopairs
+		-- Desc:automatically pair patterns like '"' '{' '['
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		config = true
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+	{
+		'vidocqh/auto-indent.nvim',
+		opts = {},
+		cond = false
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	},
+	{
+		"chrisgrieser/nvim-spider",
+		lazy = true,
+		dependencies = {
+			"theHamsta/nvim_rocks",
+			build =
+			"pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+			config = function() require("nvim_rocks").ensure_installed("luautf8") end,
+		},
+	},
+	{
+		--arrow.nvim
+		--Desc:a Neovim's bookmark plugin
+		"otavioschwanck/arrow.nvim",
+		opts = {
+			show_icons = true,
+			leader_key = ';', -- Recommended to be a single key
+			buffer_leader_key = 'm', -- Per Buffer Mappings
+		}
+	},
+	{
+		-- nvim-fundo
+		-- Desc:Makin nvim's undo more stable and useful
+		'kevinhwang91/nvim-fundo',
+		requires = 'kevinhwang91/promise-async',
+		run = function() require('fundo').install() end
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	}
 }
