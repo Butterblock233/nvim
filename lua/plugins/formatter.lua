a51e48ecfd70f672e9bcabcf199473f9c9361146
Butterblock233 <Voltage15312@outlook.com>
2025-02-14 22:38:33 +0800

    Update formatter config

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,26 +1,25 @@
-local configs = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
-		-- You can customize some of the format options for the filetype (:help conform.format)
-		rust = { "rustfmt", lsp_format = "fallback" },
-		-- Conform will run the first available formatter
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-	},
-
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 150,
-		lsp_format = "fallback",
-	},
-}
-
 return {
-	{
-		"stevearc/conform.nvim",
-		cond = false,
-		event = "VeryLazy",
-		opts = { configs },
-	}
+	"stevearc/conform.nvim",
+	event = "VeryLazy",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			python = { "isort", "black" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			rust = { "rustfmt", lsp_format = "fallback" },
+			-- Conform will run the first available formatter
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			cpp = { "clang-format" },
+		},
+	},
+	keys = {
+		{
+			"<leader>fc",
+			function()
+				require("conform").format()
+			end,
+			desc = "Format Code",
+		},
+	},
 }
