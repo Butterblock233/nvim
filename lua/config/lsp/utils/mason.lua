505e83e30c67097a4b38393340720b1ca0b4fc33
Butterblock233 <Voltage15312@outlook.com>
2025-05-09 18:52:34 +0800

    refactor(lsp): update mason setup and cold server retrieval
    - Update mason.lua to use new cold server retrieval function
    - Refactor server retrieval to use more appropriate naming conventions

--- lua/config/lsp/utils/mason.lua
+++ lua/config/lsp/utils/mason.lua
@@ -1,4 +1,4 @@
-local get_cold_servers = require("config.lsp.utils.cold_servers").get_unset_lsp_servers
+local get_cold_servers = require("utils.cold_servers").get_cold_lsp_servers
 local M = {}
 function M.setup()
 	-- 设置 mason
