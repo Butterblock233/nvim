eeca7595d43fb56ccdda4c3494c988a0133bfec6
Butterblock233 <Voltage15312@outlook.com>
2025-06-30 19:41:06 +0800

    fix(plugin) update LusSnip config
    - update LuaSnip to new version
    - update build script

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,7 +17,13 @@ return {
 			"hrsh7th/nvim-cmp",
 			{
 				"L3MON4D3/LuaSnip",
-				build = "make install_jsregexp",
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+				build = function ()
+					if vim.fn.executable("make") == 1 then
+						vim.fn.system({"make","install_jsregexp"})
+						else vim.notify("GNU Make not found. Cannot build LuaSnip jsregexp module.", vim.log.levels.WARN)
+					end
+				end
 			},
 			"saadparwaiz1/cmp_luasnip",
 			"onsails/lspkind.nvim",
