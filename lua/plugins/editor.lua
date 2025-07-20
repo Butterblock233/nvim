3076aff3e876c3d8d8079040e4a5cf36e1b077c6
Butterblock233 <Voltage15312@outlook.com>
2025-07-21 02:25:03 +0800

    feat(plugin) add flash.nvim plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,7 +7,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-neo-tree/neo-tree.nvim",
 		},
-		opts = {}
+		opts = {},
 	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
@@ -111,5 +111,19 @@ return {
 		dependencies = {
 			"TheGLander/indent-rainbowline.nvim",
 		},
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
 	},
 }
