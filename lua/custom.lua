80d780d7f91cee3f8134a534704a35a0eea81b6d
Butterblock233 <Voltage15312@outlook.com>
2025-07-21 02:24:43 +0800

    feat(shell) update nushell support

--- lua/custom.lua
+++ lua/custom.lua
@@ -1,13 +1,22 @@
 require("utils.dotenv").load(".env")
 -- vim.o.guifont = vim.env.NVIM_GUI_FONT
 vim.o.guifontwide = vim.env.NVIM_GUI_FONT_WIDE
-if vim.env.OS == "Windows_NT" or vim.env.PWSH == "true" then
-	vim.o.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
-	vim.o.shellcmdflag ="-NoLogo -NonInteractive -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+
+-- Shell settings
+if vim.env.PWSH == "true" then
+	vim.o.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+	vim.o.shellcmdflag =
+		"-NoLogo -NonInteractive -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new(); $PSDefaultParameterValues['Out-File:Encoding']='utf8'; $PSStyle.OutputRendering='plaintext'; Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
 	vim.o.shellredir = "2>&1 | ForEach-Object { '$_' } | Out-File %s; exit $LastExitCode"
 	vim.o.shellpipe = "2>&1 | ForEach-Object { '$_' } | Tee-Object %s; exit $LastExitCode"
 	vim.o.shellquote = ""
 	vim.o.shellxquote = ""
+elseif vim.env.NU == "true" then
+	vim.o.shell = vim.fn.executable("nu") == 1 and "nu" or vim.env.SHELL
+	vim.o.shellcmdflag = "-c"
+	vim.o.shellquote = ""
+	vim.o.shellxquote = ""
+	-- nushell doesn't support direct redirection via shellredir/shellpipe, fallback to default
 else
 	vim.o.shell = vim.env.SHELL
 end
