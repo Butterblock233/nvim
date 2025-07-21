41dfc39f455f60ac1c5c576e62348f3db7dd41b5
Butterblock233 <Voltage15312@outlook.com>
2025-07-22 01:09:53 +0800

    fix(treesitter) make sure treesitter highlight can enable automatically
    - this problem mainly caused by incorrect start event and config
    function

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,30 +2,14 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	tags = "0.8.5.2",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" },
+	event = { "BufReadPost", "BufNewFile" },
 	opts = {
-		ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "javascript", "html" },
+		ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "javascript", "html", "nu" },
 		sync_install = false,
-		highlight = { enable = true, event = { "BufReadPre", "BufNewFile" } },
+		highlight = { enable = true },
 		indent = { enable = true },
 	},
-	config = function(opts)
+	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
-		-- Ensure treesitter highlight for python enabled
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "python",
-			callback = function()
-				-- 启用 Tree-sitter 高亮（如果支持）
-				if vim.fn.exists(":TSBufEnable") == 2 then
-					vim.cmd("TSBufEnable highlight")
-				end
-
-				-- 可选：启用缩进（如果你也用了 nvim-treesitter 的 indent 功能）
-				-- if vim.fn.exists(":TSBufEnable") == 2 then
-				-- 	vim.cmd("TSBufEnable indent")
-				-- end
-			end,
-		})
 	end,
 }
